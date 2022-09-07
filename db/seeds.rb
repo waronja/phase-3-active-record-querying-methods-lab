e8c201df6d899114fb789c16c851bac07e15aa1c
simon waronja <simon.waronja@student.moringaschool.com>
2022-09-07 13:35:20 +0300

    Done

---
+++ db/seeds.rb
@@ -0,0 +1,6 @@
+Show.create(name: "Rick and Morty", day: "Thursday", network: "Adult Swim", rating: 10)
+Show.create(name: "Law & Order", day: "Monday", network: "NBC", rating: 7)
+Show.create(name: "The Cleveland Show", day: "Monday", network: "Fox", rating: 2)
+Show.create(name: "Fear the Walking Dead", day: "Sunday", network: "AMC", rating: 3)
+
+
